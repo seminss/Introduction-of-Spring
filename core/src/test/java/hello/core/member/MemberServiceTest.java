6084ec30f1988db119af1a2bbc962e1e40c6c4ec
semin-1432 <2007ksm@naver.com>
2021-11-07 23:48:08 +0900

    edit related AppConfig

--- core/src/test/java/hello/core/member/MemberServiceTest.java
+++ core/src/test/java/hello/core/member/MemberServiceTest.java
@@ -3,11 +3,9 @@ package hello.core.member;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 class MemberServiceTest {
 
-    MemberService memberService= new MemberServiceImpl();
+    MemberService memberService= new MemberServiceImpl(memberRepository);
 
     @Test
     void join(){
