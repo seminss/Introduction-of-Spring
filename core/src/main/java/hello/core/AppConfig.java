1a26d0513c2b79c52a5600cea4b2a1e4c73453a5
semin-1432 <2007ksm@naver.com>
2021-11-10 22:49:07 +0900

    change to spring

--- core/src/main/java/hello/core/AppConfig.java
+++ core/src/main/java/hello/core/AppConfig.java
@@ -8,22 +8,35 @@ import hello.core.member.MemberServiceImpl;
 import hello.core.member.MemoryMemberRepository;
 import hello.core.order.OrderService;
 import hello.core.order.OrderServiceImpl;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+//스프링은 앱의 구성을 나타내는 설정 정보에 @Configuration을 적어주도록 되어 있음(따라서 AppConfig가 스프링컨테이너의 구성 정보)
+//@Bean을 적은 메소드는 스프링 컨테이너에 등록이 됨
+
+@Configuration
 public class AppConfig {
+
+    //@Bean이라 적힌 메소드를 모두 호출해서 반환된 객체를 컨테이너에 등록
+    //스프링 빈은 @bean이 붙은 메서드 명을 스프링 빈의 이름으로 사용
+    @Bean
     public MemberService memberService() {
         return new MemberServiceImpl(memberRepository());
     }
 
-    private MemoryMemberRepository memberRepository() {
+    @Bean
+    public MemoryMemberRepository memberRepository() {
         return new MemoryMemberRepository();
     }
 
+    @Bean
     public OrderService orderService() {
         return new OrderServiceImpl(
                 memberRepository(),
                 discountPolicy());
     }
 
+    @Bean
     public DiscountPolicy discountPolicy(){
         //return new FixDiscountPolicy();
         return new RateDiscountPolicy();
--- core/src/main/java/hello/core/MemberApp.java
+++ core/src/main/java/hello/core/MemberApp.java
@@ -3,13 +3,23 @@ package hello.core;
 import hello.core.member.Grade;
 import hello.core.member.Member;
 import hello.core.member.MemberService;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 public class MemberApp {
 
     //psvm
     public static void main(String[] args) {
-        AppConfig appConfig=new AppConfig();
-        MemberService memberService = appConfig.memberService();
+//        AppConfig appConfig=new AppConfig();
+//        MemberService memberService = appConfig.memberService();
+
+        //ApplicationContext를 스프링 컨테이너(스프링빈)라 한다.
+        //그래서 아래와 같이 적고 AppConfig를 매개변수로 넘기면, AppConfig에 있는 환경설정 정보들(@Bean을 적은 모든 것)을 스프링 빈에 넣어준다.
+        //(앞에 Annotation이 붙는건 어노테이션을 기반으로 했다는 의미)
+        ApplicationContext applicationContext = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        //getBean을 통해 필요한 스프링 빈을 찾을 수 있다. parameter=> 1.어떤 객체를 찾을지 name => 주로 메소드명, 2.반환 타입
+        MemberService memberService= applicationContext.getBean("memberService", MemberService.class);
         Member member=new Member(1L, "memberA", Grade.VIP);
         memberService.join(member);
 
