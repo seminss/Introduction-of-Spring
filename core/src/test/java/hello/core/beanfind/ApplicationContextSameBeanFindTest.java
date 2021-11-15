597332695239e0c16bcfa70436bb8bb3cee27bd2
semin-1432 <2007ksm@naver.com>
2021-11-15 23:35:44 +0900

    Applicateion Context Extends FindTest

--- core/src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
+++ core/src/test/java/hello/core/beanfind/ApplicationContextSameBeanFindTest.java
@@ -15,7 +15,7 @@ import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
 public class ApplicationContextSameBeanFindTest {
-    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext();
+    AnnotationConfigApplicationContext ac = new AnnotationConfigApplicationContext(SameBeanConfig.class);
 
     @Test
     @DisplayName("타입으로 조회시 같은 타입이 둘 이상 있으면, 중복 오류가 발생한다.")
